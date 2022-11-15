434a246e8a5c3278ad5f2e3a8f2dc7f0bc930bc6
Parvathy <116579961+106263@users.noreply.github.com>
2022-11-15 13:59:23 +0000

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">parvathy10626316 !</p>
+                <p className="text-center">haiparvathyare you ok !</p>
         
             {items.map(item => (
                 <ItemDisplay
